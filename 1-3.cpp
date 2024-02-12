c18797b2bf1e2c4ee18d09f8667acea0dd9b4b2f
hashep <gooboo712@gmail.com>
2024-02-12 17:34:12 +0900

    131

---
+++ 1-3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int whatisthis = 2;
+    int user_input;
+
+    cin >> user_input;
+
+    switch (user_input)
+    {
+    case 1:
+        cout << "1번" << endl;
+        break;
+
+    case 224:
+        cout << "시발" << endl;
+        break;
+    }
+
+    // d
+    int a;
+    std::cin >> a;
+}
+
+/*
+변수 이름의 맨 앞부분에는 숫자가 오면 안됩니다.
+변수 이름이 조금 길더라도 확실히 이해할 수 있는 변수 이름을 짓는 것이 매우 중요합니다.
+*/
